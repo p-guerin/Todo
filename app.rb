05e47cbf28f82845cca91d5fc4f993599932ada8
p-guerin <guerin.pierre26@gmail.com>
2015-05-24 12:56:31 +0200

    Create get route app.rb

--- app.rb
+++ app.rb
@@ -3,3 +3,16 @@ require 'sinatra/activerecord'
 require 'sinatra/flash'
 require 'sinatra/redirect_with_flash'
 require './environments'
+
+ActiveRecord::Base.default_timezone = :local
+
+class Todo < ActiveRecord::Base
+end
+
+get "/" do
+  @title = "Todo!"
+  @todo = Todo.where(:stage => 0)
+  @doing = Todo.where(:stage => 1)
+  @done = Todo.where(:stage => 2)
+  erb :todo
+end
