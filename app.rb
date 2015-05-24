73f189354560d7a765d62db651b87245a60b4aa6
p-guerin <guerin.pierre26@gmail.com>
2015-05-24 14:37:37 +0200

    Add route post, put and delete to the app.rb

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require './environments'
 ActiveRecord::Base.default_timezone = :local
 
 class Todo < ActiveRecord::Base
+    validates :task, presence: true, length: { minimum: 1 }
 end
 
 get "/" do
@@ -15,4 +16,22 @@ get "/" do
   @doing = Todo.where(:stage => 1)
   @done = Todo.where(:stage => 2)
   erb :todo
+end
+
+post "/" do
+  @todos = Todo.new(params[:todos])
+  @todos.save
+  redirect "/"
+end
+
+put "/:id" do
+  @todos = Todo.find(params[:id])
+  @todos.update_attributes(params[:todos])
+  redirect "/"
+end
+
+delete "/:id" do
+  @todos = Todo.find(params[:id])
+  @todos.destroy
+  redirect "/"
 end
